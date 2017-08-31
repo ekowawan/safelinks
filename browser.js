286961595ef7980014ebaa9fc659964732981dfb
ekowawan <31313477+ekowawan@users.noreply.github.com>
2017-09-01 01:47:00 +0700

    Create browser.js

---
+++ browser.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+//<![CDATA[
+var qzxcpsd = navigator.userAgent;
+var redirect = qzxcpsd.search("UCBrowser");
+if(redirect>1) {
+var xvrzqw = window.location.assign("googlechrome://navigate?url="+ window.location.href);
+var activity = xvrzqw;document.getElementsByTagName('head')[0].appendChild(activity);
+}
+//]]>
+</script>
